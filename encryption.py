e9d70aa97e1a6ec5dfbcc397d704a5606aac0efb
jkim99 <jonathanskim@gmail.com>
2018-10-13 13:31:00 -0400

    initialization done

--- encryption.py
+++ encryption.py
@@ -1,18 +1,34 @@
 import math
-
-
-cube = []
+import random
 
 
 def main():
     """
     """
+    array = ['7', 'A', '3', 'F']
+    c = Cube(array=array)
 
 
-def append_to_cube(array):
-    """
-    """
-    int(len(array) ** (1 / 3)) + 1
+class Cube():
+    def __init__(self, *args, **kwargs):
+        """
+        """
+        self.cube = []
+        cube_dem = int(len(kwargs['array']) ** (1 / 3)) + 1
+        x = 0
+        for i in range(0, cube_dem):
+            self.cube.append([])
+            for j in range(0, cube_dem):
+                self.cube[i].append([])
+                for k in range(0, cube_dem):
+                    try:
+                        self.cube[i][j].append(kwargs['array'][x])
+                    except IndexError:
+                        self.cube[i][j].append(chr(int(
+                            random.uniform(71, 90))))
+                    x += 1
+        print('Cube created with a side length of ' + str(cube_dem))
+        print(self.cube)
 
 
 if __name__ == '__main__':
